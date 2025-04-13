77f16f6ac443e7e0f171810af5d6724d8045ea53
Thi4goFar <thi4go.faria@gmail.com>
2025-04-13 17:21:08 -0300

    added tests

--- tests/reservation_tests.py
+++ tests/reservation_tests.py
@@ -10,15 +10,16 @@ BASE_RESERVATION = {
     "client_name": "John Doe",
     "client_email": "john@example.com",
     "guest_quantity": 2,
-    "active": True
+    "active": True,
 }
 CAPACITY_CONFLICT_RESERVATION = {
     "property_id": 2,
     "client_name": "John Doe",
     "client_email": "john@example.com",
     "guest_quantity": 7,
-    "active": True
+    "active": True,
 }
+
 
 @pytest.mark.anyio
 async def test_root():
@@ -29,6 +30,7 @@ async def test_root():
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to South lake zone API"}
 
+
 @pytest.mark.asyncio
 async def when_dates_are_invalid_should_return_error():
     start_date = datetime.now()
@@ -36,13 +38,12 @@ async def when_dates_are_invalid_should_return_error():
     payload = {
         **BASE_RESERVATION,
         "start_date": start_date.isoformat(),
-        "end_date": end_date.isoformat()
+        "end_date": end_date.isoformat(),
     }
     async with AsyncClient(
-            transport=ASGITransport(app=app), base_url="http://test"
+        transport=ASGITransport(app=app), base_url="http://test"
     ) as client:
         response = await client.post("/reservation/", json=payload)
-
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert "end date precedes start date" in response.text.lower()
@@ -56,14 +57,20 @@ async def when_guest_number_exceeds_capacity_should_return_error(mocker):
     payload = {
         **CAPACITY_CONFLICT_RESERVATION,
         "start_date": start_date.isoformat(),
-        "end_date": end_date.isoformat()
+        "end_date": end_date.isoformat(),
     }
 
-    mocker.patch("commands.command_handlers.property_handlers.find_availability_conflict", return_value=False)
-    mocker.patch("commands.command_handlers.property_handlers.find_capacity_conflict", return_value=True)
+    mocker.patch(
+        "commands.command_handlers.property_handlers.find_availability_conflict",
+        return_value=False,
+    )
+    mocker.patch(
+        "commands.command_handlers.property_handlers.find_capacity_conflict",
+        return_value=True,
+    )
 
     async with AsyncClient(
-            transport=ASGITransport(app=app), base_url="http://test"
+        transport=ASGITransport(app=app), base_url="http://test"
     ) as client:
         response = await client.post("/reservation/", json=payload)
     assert response.status_code == status.HTTP_409_CONFLICT
